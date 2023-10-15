c574d3f77ddeb778f286c1a661f1483d18baeb3b
Favouriteless <62647254+Favouriteless@users.noreply.github.com>
2023-10-15 03:42:35 +0100

    fixed the db being overwritten

--- src/util/config.ts
+++ src/util/config.ts
@@ -10,5 +10,5 @@ interface Config {
 export const config: Config = require('../../data/config.json'); // This path is relative to THIS file.
 
 export function saveConfig() {
-    fs.writeFileSync(path.join(__dirname, '../../data/database.db'), JSON.stringify(config, undefined, 1)); // The path here is relative to the "root" file, ie elaina.ts or elaina.js (prod).
+    fs.writeFileSync(path.join(__dirname, '../../data/config.json'), JSON.stringify(config, undefined, 1)); // The path here is relative to the "root" file, ie elaina.ts or elaina.js (prod).
 }
