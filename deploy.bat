785756386c9dc237a9dc361bef140813188e4be3
Favouriteless <62647254+Favouriteless@users.noreply.github.com>
2026-01-23 21:11:09 +0000

    Deployment changes
    - Updated compose files for deployments
    - Removed deploy.bat, it is unnecessary now
    - Added GH action for build & publish on release added

--- deploy.bat
+++
@@ -1,5 +0,0 @@
-docker build . -t elaina:latest
-
-docker image save elaina:latest -o build/image
-scp .\build\image favouriteless@maven.favouriteless.net:/srv/favouriteless-maven/image
-DEL /F /Q build
