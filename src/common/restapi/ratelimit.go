9238bfc729bf6f98d5bd595ee81ce7c8c0fd31ad
Favouriteless <62647254+Favouriteless@users.noreply.github.com>
2026-02-03 20:11:01 +0000

    Ratelimit change
    - Removed the per-bucket async nature of buckets. It worked, but it was way too complex to maintain.
    
    - Removed global synchronous requests in favour of a per-route synchronous request

--- src/common/restapi/ratelimit.go
+++ src/common/restapi/ratelimit.go
@@ -1,6 +1,7 @@
 package restapi
 
 import (
+	"math"
 	"net/http"
 	"strconv"
 	"sync"
@@ -20,40 +21,33 @@ type routeBucket struct {
 
 // consume will block until the bucket has a token available for consumption, then consume it. If true is returned, the
 // bucket should only be unlocked after an http response for it has been returned.
-func (b *routeBucket) consume() (deferUnlock bool) {
+func (b *routeBucket) consume() {
 	b.Lock()
-	defer func() {
-		if !deferUnlock {
-			b.Unlock()
-		}
-	}()
 
 	b.mu.Lock()
 	defer b.mu.Unlock()
 
-	wait := b.reset.Sub(time.Now())
-
-	if b.remaining > 0 {
-		b.remaining--
-		// If the bucket is now empty & does not have a new reset time, unlocking must be deferred until after
-		// a response.
-		return b.remaining == 0 && wait < 0
-	}
-
-	// If reset is in the future, we'll wait until then and provisionally assign ourselves 50% of the tokens from last time.
-	// This allows for lower overall latency, but could introduce more 429s. May change in future.
-	if wait > 0 {
-		b.mu.Unlock() // Temporarily unlock bucket contents while sleeping
+	// If reset is in the future, we have to wait until then.
+	if wait := b.getWaitTime(); wait > 0 {
 		time.Sleep(wait)
-		b.mu.Lock()
-		b.remaining = b.limit / 2
 	}
 
 	b.remaining--
-	return false
+}
+
+// getWaitTime calculates the amount of time the bucket needs to wait to be able to access a token
+func (b *routeBucket) getWaitTime() time.Duration {
+	if b.remaining > 0 {
+		return 0
+	}
+	return b.reset.Sub(time.Now())
 }
 
 func (b *routeBucket) update(headers http.Header) error {
+	if headers == nil {
+		return nil
+	}
+
 	hLimit := headers.Get("X-RateLimit-Limit")
 	hRemaining := headers.Get("X-RateLimit-Remaining")
 	hReset := headers.Get("X-RateLimit-Reset")
@@ -86,7 +80,13 @@ func (b *routeBucket) update(headers http.Header) error {
 		}
 		b.reset = time.Now().Add(time.Duration(resetAfter * float64(time.Second)))
 	} else if hReset != "" {
+		reset, err := strconv.ParseFloat(hReset, 64)
+		if err != nil {
+			return err
+		}
 
+		whole, frac := math.Modf(reset)
+		b.reset = time.Unix(int64(whole), int64(frac*float64(time.Second)))
 	}
 	return nil
 }
